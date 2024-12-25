9ae6bb69b95d8fa7e42fcb16647840ca2a12aa61
Yojhi <yoshitani.takumi.g9@s.gifu-u.ac.jp>
2024-12-25 22:26:44 +0900

    update

--- odqlab_1.0.0a/odqcost.m
+++ odqlab_1.0.0a/odqcost.m
@@ -8,25 +8,6 @@ function Efinal = odqcost(G,Q,d,steptime,plotflag)
 %
 %See also compg, odq, odqgain, odqreal, odqstb.
 
-% -------------------------------------------------------------------------
-% Copyright is with the following author. 
-% (C) 2008 Ryosuke Morita, 
-%          Kyoto University;
-%          Gokasho, Uji, Kyoto 611-0011, Japan
-%          morita@robot.kuass.kyoto-u.ac.jp
-% -------------------------------------------------------------------------
-% Legal Note:
-%           
-%     (a)  This program is a free software. 
-%          
-%     (b)  This program is distributed according to GNU General Public
-%          License, i.e., it is allowed to use WITHOUT ANY WARRANTY; 
-%          without even the implied warranty of
-%          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-%          See the GNU General Public License for more details.
-% 
-% -------------------------------------------------------------------------
-
 %%%%%set default steptime%%%%%
 if (nargin==5)
     if isempty(steptime)==1
@@ -40,6 +21,15 @@ elseif (nargin==3)
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+if isinf(steptime)
+    strarch=computer('arch');
+    switch strarch
+        case {'win64' , 'glnxa64' , 'maci64'}
+            steptime=double(intmax('int64'));
+        otherwise
+            steptime=double(intmax('int32'));
+    end
+end
 finaltime=steptime;      %initialize
 
 G.aa = G.a+G.b2*G.c2;    %convert to closed loop (only G)
@@ -71,87 +61,49 @@ sum_CAB = zeros( size( G.c1 , 1 ) , size( G.b2 , 2 ) );     %initialize
 %%%%%%%%%%computing gain%%%%%%%%%%
 disp('Computing cost...')
 fprintf('%s','0%...')
-if plotflag=='t'
-    for i=1:steptime
-        sum_CAB = sum_CAB + abs( Cbar * Abar^(i-1) * Bbar );
-        E(i) = norm(sum_CAB,inf)*d/2;
-        T(i) = i;
-        if (i>1)
-            if (abs(E(i)-E(i-1))<1e-8)
-                finaltime=i;
-                %T=eye(finaltime,steptime)*T;
-                %E=eye(finaltime,steptime)*E;
-                break
-            end
+for i=1:steptime
+    sum_CAB = sum_CAB + abs( Cbar * Abar^(i-1) * Bbar );
+    E(i) = norm(sum_CAB,inf)*d/2;
+    T(i) = i;
+    if (i>1)
+        if (abs(E(i)-E(i-1))<1e-8)
+            finaltime=i;
+%            T=eye(finaltime,steptime)*T;
+%            E=eye(finaltime,steptime)*E;
+            break
         end
-        %%%%%%%%%%display progress%%%%%%%%%%
-        if i==floor(steptime/10)
-            fprintf('%s','10%...')
-        elseif i==floor(steptime/10)*2
-            fprintf('%s','20%...')
-        elseif i==floor(steptime/10)*3
-            fprintf('%s','30%...')
-        elseif i==floor(steptime/10)*4
-            fprintf('%s','40%...')
-        elseif i==floor(steptime/10)*5
-            fprintf('%s','50%...')
-        elseif i==floor(steptime/10)*6
-            fprintf('%s','60%...')
-        elseif i==floor(steptime/10)*7
-            fprintf('%s','70%...')
-        elseif i==floor(steptime/10)*8
-            fprintf('%s','80%...')
-        elseif i==floor(steptime/10)*9
-            fprintf('%s','90%...')
-        end
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %pause(0.001)
     end
-    fprintf('%s\n','Finish!')
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    Efinal = E(finaltime);      %RESULT
-    
-    %%%%%%%%%%ploting%%%%%%%%%%
-    if (plotflag=='t')
-        figure(1)
-        plot(T,E);
+    %%%%%%%%%%display progress%%%%%%%%%%
+    if i==floor(steptime/10)
+        fprintf('%s','10%...')
+    elseif i==floor(steptime/10)*2
+        fprintf('%s','20%...')
+    elseif i==floor(steptime/10)*3
+        fprintf('%s','30%...')
+    elseif i==floor(steptime/10)*4
+        fprintf('%s','40%...')
+    elseif i==floor(steptime/10)*5
+        fprintf('%s','50%...')
+    elseif i==floor(steptime/10)*6
+        fprintf('%s','60%...')
+    elseif i==floor(steptime/10)*7
+        fprintf('%s','70%...')
+    elseif i==floor(steptime/10)*8
+        fprintf('%s','80%...')
+    elseif i==floor(steptime/10)*9
+        fprintf('%s','90%...')
     end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%
-else
-    for i=1:steptime
-        sum_CAB = sum_CAB + abs( Cbar * Abar^(i-1) * Bbar );
-        E = norm(sum_CAB,inf)*d/2;
-        if (i>1)
-            if (abs(E-Ebefore)<1e-8)
-                finaltime=i;
-                break
-            end
-        end
-        %%%%%%%%%%display progress%%%%%%%%%%
-        if i==floor(steptime/10)
-            fprintf('%s','10%...')
-        elseif i==floor(steptime/10)*2
-            fprintf('%s','20%...')
-        elseif i==floor(steptime/10)*3
-            fprintf('%s','30%...')
-        elseif i==floor(steptime/10)*4
-            fprintf('%s','40%...')
-        elseif i==floor(steptime/10)*5
-            fprintf('%s','50%...')
-        elseif i==floor(steptime/10)*6
-            fprintf('%s','60%...')
-        elseif i==floor(steptime/10)*7
-            fprintf('%s','70%...')
-        elseif i==floor(steptime/10)*8
-            fprintf('%s','80%...')
-        elseif i==floor(steptime/10)*9
-            fprintf('%s','90%...')
-        end
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %pause(0.001)
-        Ebefore=E;
-    end
-    fprintf('%s\n','Finish!')
-    Efinal = E;      %RESULT
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+pause(0.001)
 end
+fprintf('%s\n','Finish!')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Efinal = E(finaltime);      %RESULT
+
+%%%%%%%%%%ploting%%%%%%%%%%
+if (plotflag=='t')
+    figure(1)
+    plot(T,E);
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
